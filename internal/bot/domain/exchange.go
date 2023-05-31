a553f91c0bea3d5eb3b7af9ae52885f0b0650d6e
breedish <breedish@gmail.com>
2023-05-31 16:39:01 +0200

    tbot #2: Init interface

--- internal/bot/domain/exchange.go
+++ internal/bot/domain/exchange.go
@@ -3,14 +3,25 @@ package domain
 import "time"
 
 type Exchange interface {
-	GetInfo() ExchangeInfo
-	GetOrderState(ID string) ExchangeOrderState
+	GetInfo() (*ExchangeInfo, error)
+	GetOrderState(ID string) (*ExchangeOrderState, error)
 }
 
-type ExchangeOrderState interface {
+type ExchangeOrderState struct {
 }
 
 type ExchangeInfo struct {
 	Quote float32
 	Epoch time.Time
+}
+
+type mockExchange struct {
+}
+
+func (e *mockExchange) GetInfo() (*ExchangeInfo, error) {
+	return &ExchangeInfo{0.0, time.Now()}, nil
+}
+
+func (e *mockExchange) GetOrderState(ID string) (*ExchangeOrderState, error) {
+	return &ExchangeOrderState{}, nil
 }
